102f3b932eed48933c185f4952ca7ce0637ce984
Daniel Stockman <daniel.stockman@gmail.com>
2008-04-17 10:26:34 -0700

    Let's pretend to follow WP coding style guide

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
 // gallery  ====================================
-function widget_race_gallery($args) {
-	extract($args);
+function widget_race_gallery( $args ) {
+	extract( $args );
 
 	$gallery = build_race_gallery();
 
-	if ($gallery) {
+	if ( $gallery ) {
 		echo $before_widget . "\n\t\t\t\t" . $gallery . $after_widget;
 	}
 }
@@ -23,24 +23,24 @@ function build_race_gallery() {
 		'size'       => 'thumbnail',
 	));
 
-	$id = intval($id);
-	$orderby = addslashes($orderby);
-	$attachments = get_children("post_parent=$id&post_type=attachment&post_mime_type=image&orderby=\"{$orderby}\"");
+	$id = intval( $id );
+	$orderby = addslashes( $orderby );
+	$attachments = get_children( "post_parent=$id&post_type=attachment&post_mime_type=image&orderby=\"{$orderby}\"" );
 
-	if ( empty($attachments) )
+	if ( empty( $attachments ) )
 		return '';
 
-	$itemtag = tag_escape($itemtag);
-	$captiontag = tag_escape($captiontag);
-	$columns = intval($columns);
+	$itemtag    = tag_escape( $itemtag );
+	$captiontag = tag_escape( $captiontag );
+	$columns    = intval( $columns );
 
-	$output = apply_filters('gallery_style', '<ul class="gallery">');
+	$output = apply_filters( 'gallery_style', '<ul class="gallery">' );
 
 	foreach ( $attachments as $id => $attachment ) {
-		$link = wp_get_attachment_link($id, $size);
+		$link = wp_get_attachment_link( $id, $size );
 		$output .= "\n\t\t\t\t\t<{$itemtag} class='gallery-item'>";
 		$output .= "\n\t\t\t\t\t\t{$link}";
-		if ( $captiontag && trim($attachment->post_excerpt) ) {
+		if ( $captiontag && trim( $attachment->post_excerpt ) ) {
 			$output .= "\n\t\t\t\t\t\t<{$captiontag} class='gallery-caption'>{$attachment->post_excerpt}</{$captiontag}>";
 		}
 		$output .= "\n\t\t\t\t\t</{$itemtag}>";
@@ -51,31 +51,31 @@ function build_race_gallery() {
 	return $output;
 }
 
-function race_gallery($attr) {
+function race_gallery( $attr ) {
 	return '<!-- gallery in sidebar -->';
 }
 
 add_filter('post_gallery', 'race_gallery');
 
 // sub menu ====================================
-function widget_race_submenu($args) {
-	extract($args);
+function widget_race_submenu( $args ) {
+	extract( $args );
 
 	global $post;
 
 	$list_ops = "title_li=&echo=0&sort_column=menu_order&child_of=";
 
-	if ($post->post_parent)
+	if ( $post->post_parent )
 		$list_ops .= $post->post_parent;
 	else
 		$list_ops .= $post->ID;
 
-	$children = wp_list_pages($list_ops);
+	$children = wp_list_pages( $list_ops );
 
-	if ($children) {
+	if ( $children ) {
 		echo $before_widget . "\n"; ?>
 				<ul class="submenu-parent">
-					<?php echo trim(implode("\n\t\t\t\t\t", explode("\n", $children))) . "\n"; ?>
+					<?php echo trim( implode( "\n\t\t\t\t\t", explode( "\n", $children ) ) ) ."\n"; ?>
 				</ul><?php
 		echo $after_widget;
 	}
@@ -83,36 +83,36 @@ function widget_race_submenu($args) {
 
 // theme widget init ===========================
 function race_widget_init() {
-	if (!function_exists('register_sidebars'))
+	if ( !function_exists( 'register_sidebars' ) )
 		return;
 
 	$widget_ops = array(
-		'classname'    =>  'widget_submenu',
-		'description'  =>  "A submenu that displays children of the current page."
+		'classname'   => 'widget_submenu',
+		'description' => "A submenu that displays children of the current page."
 	);
 
-	wp_register_sidebar_widget('submenu', 'Page Sub-menu', 'widget_race_submenu', $widget_ops);
+	wp_register_sidebar_widget( 'submenu', 'Page Sub-menu', 'widget_race_submenu', $widget_ops );
 
-	$widget_ops['classname'] = 'widget_gallery';
+	$widget_ops['classname']   = 'widget_gallery';
 	$widget_ops['description'] = "A sidebar image gallery using shortcodes for post";
 
-	wp_register_sidebar_widget('sidegallery', 'Page Gallery', 'widget_race_gallery', $widget_ops);
+	wp_register_sidebar_widget( 'sidegallery', 'Page Gallery', 'widget_race_gallery', $widget_ops );
 }
 
-add_action('widgets_init', 'race_widget_init');
+add_action( 'widgets_init', 'race_widget_init' );
 
 // theme utilities =============================
 function race_header() {
-	$root = get_bloginfo('stylesheet_directory');
+	$root = get_bloginfo( 'stylesheet_directory' );
 
 	// because IE is a friggin retard
 	echo <<<HTML
 <!--[if lte IE 6]><link rel="stylesheet" type="text/css" href="{$root}/ie.css" /><![endif]-->
 HTML;
 
-	wp_enqueue_script('race', $root . "/util.js", array('jquery'));
+	wp_enqueue_script( 'race', $root . "/util.js", array('jquery') );
 }
 
-add_action('wp_print_scripts', 'race_header');
+add_action( 'wp_print_scripts', 'race_header' );
 
 ?>
