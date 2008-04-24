62ccb52d5c050d8609dcb517d4134a09a4cad6f5
Daniel Stockman <daniel.stockman@gmail.com>
2008-04-24 15:40:03 -0700

    User profile stuff

--- profile.php
+++ profile.php
@@ -7,9 +7,8 @@
 			<div id="profile" class="<?php sandbox_post_class() ?>">
 				<h2 class="profile-title"><?php aleph_the_user_complete_name(); ?></h2>
 				<div class="entry-content">
-<?php aleph_the_user_avatar('<div class="user-avatar">', '</div>'); ?>
+<?php aleph_the_user_photo('<div class="user-image">', '</div>'); ?>
 <?php aleph_the_user_description(); ?>
-
 				</div>
 			</div><!-- .post -->
 <?php } else { ?>
@@ -17,6 +16,10 @@
 
 		<p>We couldn't found the user you're looking for.</p>
 <?php } ?>
+		<div id="race-summary">
+			<h4>RACE Summary</h4>
+			<p>RACE Charities is a non-profit organization dedicated to raising awarenes of cancer through early detection.  RACE organizes events around the country in efforts to raise awareness of the importance of early detection of cancer, especially for young people with a family history of cancer.  RACE was founded by Glory Gensch (pictured on top of Page) who passed away from colon cancer at the age of 23.  Glory’s legacy and mission lives on through RACE Charities, whose fundraising campaign is focused on raising money to donate directly to nationally renowned early detection research institutes around the world.  Glory, as well as all RACE volunteers and members, believe that if our efforts can save just one life, then it is well worth the sacrifice!</p>
+		</div>
 
 		</div><!-- #content -->
 
--- util.js
+++ util.js
@@ -5,5 +5,10 @@
 			$('#blog-description').text().replace(/\b([A-Z])/g, '<span class="acro">$1</span>')
 		);
 		$('table.zebra tr:odd').addClass('alt');
+		/* hiding login/signup links when already logged in
+		$('body.loggedin #submenu li.page_item').filter(function(i) {
+			return !$('a', this).attr('title').search(/^(signup|login)$/i);
+		}).css('display','none');
+		//*/
 	});
 })(jQuery);
