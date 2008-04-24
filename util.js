130efca93edce1103ced5d3f72a65dec5585da6b
Daniel Stockman <daniel.stockman@gmail.com>
2008-04-23 17:01:36 -0700

    Front page tweaks (title tooltip, text size and image interplay)

--- util.js
+++ util.js
@@ -1,5 +1,6 @@
 (function($) {
 	$(function() {
+		$('#header h1 a').attr('title','Click to return to RACE Charities homepage');
 		$('#blog-description').html(
 			$('#blog-description').text().replace(/\b([A-Z])/g, '<span class="acro">$1</span>')
 		);
