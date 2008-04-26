c58bb68997285a1574bc8894158a9b430b43ca6f
Daniel Stockman <daniel.stockman@gmail.com>
2008-04-26 01:25:08 -0700

    facebox login / signup shenanigans

--- util.js
+++ util.js
@@ -5,10 +5,33 @@
 			$('#blog-description').text().replace(/\b([A-Z])/g, '<span class="acro">$1</span>')
 		);
 		$('table.zebra tr:odd').addClass('alt');
-		/* hiding login/signup links when already logged in
-		$('body.loggedin #submenu li.page_item').filter(function(i) {
-			return !$('a', this).attr('title').search(/^(signup|login)$/i);
-		}).css('display','none');
+
+		//* login/signup link tomfoolery (requires wp-facebox)
+		if ('undefined' != typeof WPFB) {
+			var loggedin = $(document.body).hasClass('loggedin');
+
+			$('#submenu li.page_item a').filter(function(i) {
+				return !$(this).attr('title').search(/^(signup|login)$/i);
+			}).each(function(i) {
+				var iz = this.title.toLowerCase();
+				var path = WPFB.site;
+				switch (iz) {
+				case 'signup':
+					path += (loggedin) ? '/wp-admin/profile.php' : '/wp-login.php?action=register';
+					if (loggedin) $(this).text('Profile');
+				break;
+				case 'login':
+					path += '/wp-login.php' + (loggedin ? '?action=logout' : '');
+					if (loggedin) $(this).text('Logout');
+				break;
+				}
+				$(this)
+					.attr({
+						/*'rel': 'facebox.' + iz,*/
+						'href': path
+					});
+			});
+		}
 		//*/
 	});
 })(jQuery);
