48df84c3277a596fb8537e4b4d59db16729aab93
Daniel Stockman <daniel.stockman@gmail.com>
2008-04-30 19:50:56 -0700

    Profile UI tweaks (make the donate button bigger)

--- profile.php
+++ profile.php
@@ -5,9 +5,9 @@
 
 <?php if (have_users()) { the_user(); ?>
 			<div id="profile" class="<?php sandbox_post_class() ?>">
-				<h2 class="profile-title"><?php aleph_the_user_complete_name(); ?></h2>
-				<div class="entry-content" id="user-<?php aleph_the_user_ID(); ?>">
+				<h2 class="profile-title"><span><?php aleph_the_user_complete_name(); ?></span> is a Warrior and on a mission to <em>Fight For Life</em>!</h2>
 <?php aleph_the_user_photo('<div class="user-image">', '</div>'); ?>
+				<div class="entry-content" id="user-<?php aleph_the_user_ID(); ?>">
 <?php aleph_the_user_description(); ?>
 				</div>
 			</div><!-- .post -->
