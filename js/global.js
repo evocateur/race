4054f55064c7a0273941286575bf9accdf5567c3
Daniel Stockman <daniel.stockman@gmail.com>
2008-05-13 18:49:59 -0700

    Moved zebra styles into donor-list plugin

--- js/global.js
+++ js/global.js
@@ -51,7 +51,6 @@ jQuery(function($) {
 	$('#blog-description').html(
 		$('#blog-description').text().replace(/\b([A-Z])/g, '<span class="acro">$1</span>')
 	);
-	$('table.zebra tr:odd').addClass('alt');
 
 	if ($.fieldValue) {	// only pages with ajax have forms plugin included
 
